fee656c71fb5a5e305bc827f85c6fb4579334055
marioanloru <marioanloru@gmail.com>
2021-05-05 23:25:41 +0200

    Precision not needed as argument now. Flow stopped when target has been reached. Avoid opening new position if stop loss did not trigger

--- liquidity.py
+++ liquidity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # DOC: https://binance-docs.github.io/apidocs/futures/en/#continuous-contract-kline-candlestick-data
-# Usage: python3 liquidity.py --pair XMR --quantity 10 --interval HOUR --leverage 2 --precision 2
+# Usage: python3 liquidity.py --pair XMR --quantity 10 --interval HOUR --leverage 2 
 import sys
 import requests
 import time
@@ -23,15 +23,15 @@ load_dotenv()
 API_KEY = os.environ.get('API_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-MAX_ORDER_RETRIES = 2
+MAX_ORDER_RETRIES = 3
 
 SLEEP_TIMEOUT = 30
 START_INTERVAL = 0
 END_INTERVAL = 8
 
-MAX_STOP_LOSS_RISK = 4
+MAX_STOP_LOSS_RISK = 3
 
-candle_status_green = False
+INITIAL_DELAY = False
 
 # Futures environment variables
 BINANCE_FUTURES_BASE_URL = "https://fapi.binance.com"
@@ -46,6 +46,9 @@ BINANCE_SPOT_EXCHANGE_INFO_ENDPOINT = "/api/v3/exchangeInfo"
 TIMES_GREEN = 0
 LAST_CANDLE_RED = True
 LAST_LOW_PRICE = 999999
+
+STOP_LOSS_REACHED = False
+STOP_LOSS = 0
 
 TARGET_REACHED = False
 TARGET = 99999
@@ -115,15 +118,24 @@ class Markets(Enum):
 
 
 def check_open_trade_ready():
+    global INITIAL_DELAY
     now = datetime.utcnow()
     hour_check = now.hour >= START_INTERVAL and now.hour <= END_INTERVAL
     if (hour_check):
         print(yellow("\nChecking candle open: {} -> {}.".format(now.strftime('%B %d %Y - %H:%M:%S'), hour_check)))
+        if (not INITIAL_DELAY):
+            print('Espero 10 segundos antes de pedir la primera vez')
+            time.sleep(10)
+            INITIAL_DELAY = True
     else:
         print(yellow("\nChecking candle open: {} -> {}. Checking again in {} seconds.".format(now.strftime('%B %d %Y - %H:%M:%S'), hour_check, SLEEP_TIMEOUT)))
     return hour_check
 
-def open_long_position_binance_futures(pair, take_profit, stop_loss, pair_change, quantity, leverage, precision):
+def open_long_position_binance_futures(pair, take_profit, stop_loss, pair_change, quantity, leverage):
+    global STOP_LOSS
+    global TARGET
+    global STOP_LOSS_REACHED
+
     request_client = RequestClient(api_key=API_KEY, secret_key=SECRET_KEY)
     # Change leverage
     try:
@@ -135,19 +147,28 @@ def open_long_position_binance_futures(pair, take_profit, stop_loss, pair_change
         margin_type = request_client.change_margin_type(symbol=pair, marginType=FuturesMarginType.ISOLATED)
     except:
         print(red.bold('error changing margin type'))
-    # Create order
-    quantity_rounded = float(quantity * leverage) / float(pair_change)
-    quantity_with_precision = "{:0.0{}f}".format(quantity_rounded, precision)
     
     # Request info of all symbols to retrieve precision
     exchange_info = request_client.get_exchange_information()
     price_precision = 0
     for item in exchange_info.symbols:
         if (item.symbol == pair):
+            print(dir(item))
+            print(item)
+            precision = item.quantityPrecision
             price_precision = item.pricePrecision
 
+    # Create order
+    quantity_rounded = float(quantity * leverage) / float(pair_change)
+    quantity_with_precision = "{:0.0{}f}".format(quantity_rounded, precision)
+    
     stop_loss = "{:0.0{}f}".format(stop_loss, price_precision)
     take_profit = "{:0.0{}f}".format(take_profit, price_precision)
+
+    STOP_LOSS = stop_loss
+    STOP_LOSS_REACHED = False
+
+    TARGET = take_profit
 
     print(white.bold('\n\tOpening future position LONG at market ({}) with quantity: {} {} with take profit on: {} and stop loss: {}'.format(pair_change, quantity_with_precision, pair, take_profit, stop_loss)))
     result = request_client.post_order(symbol=pair, side=OrderSide.BUY, quantity=quantity_with_precision, ordertype=OrderType.MARKET, positionSide="BOTH")
@@ -165,7 +186,7 @@ def open_long_position_binance_futures(pair, take_profit, stop_loss, pair_change
         request_client.cancel_order(symbol=pair, orderId=orderId)
         print(red.bold('\n\t\t x Something did not work as expected. Cancelling order'))
 
-def open_position_binance_spot(pair, limit, pair_change, quantity, precision, side = SpotSides.BUY):
+def open_position_binance_spot(pair, limit, pair_change, quantity, side = SpotSides.BUY):
     url = BINANCE_SPOT_BASE_URL + BINANCE_SPOT_CREATE_ORDER_ENDPOINT
     
     response = requests.get(BINANCE_SPOT_BASE_URL + BINANCE_SPOT_EXCHANGE_INFO_ENDPOINT)
@@ -224,13 +245,21 @@ def minimum_downside(cc_open, cc_low):
     downside = (diff / cc_low) * 100
     return downside > 0.5
 
-def trade_the_open(pair, interval, quantity, leverage, precision, market, limit):
+def trade_the_open(pair, interval, quantity, leverage, market, limit):
     global LAST_CANDLE_RED
     global LAST_LOW_PRICE
     global TIMES_GREEN
     global TARGET
     global TARGET_REACHED
-    candles = get_last_binance_candles(pair, interval, market)
+    global STOP_LOSS_REACHED
+    global STOP_LOSS
+
+    try:
+        candles = get_last_binance_candles(pair, interval, market)
+    except:
+        time.sleep(SLEEP_TIMEOUT)
+        candles = get_last_binance_candles(pair, interval, market)
+            
     """ Binance API response format
     [
         [
@@ -265,6 +294,12 @@ def trade_the_open(pair, interval, quantity, leverage, precision, market, limit)
     if (cc_high >= TARGET):
         TARGET_REACHED = True
 
+    if (cc_low <= STOP_LOSS):
+        STOP_LOSS_REACHED = True
+
+    if (TIMES_GREEN > 1 and not STOP_LOSS_REACHED):
+        return False
+
     if (cc_open < cc_close and cc_open >= cc_low):
         if (LAST_CANDLE_RED and cc_low < LAST_LOW_PRICE):
             print('***** INTENTO NUMERO: {} ******'.format(TIMES_GREEN))
@@ -288,10 +323,9 @@ def trade_the_open(pair, interval, quantity, leverage, precision, market, limit)
 
         if (check_safe_stop_loss(cc_low, cc_open)):
             if (market == Markets.FUTURES):
-                TARGET = targets["tp1"]
-                open_long_position_binance_futures(pair, targets["tp1"], cc_low, cc_close, quantity, leverage, precision)
+                open_long_position_binance_futures(pair, targets["tp3"], cc_low, cc_close, quantity, leverage)
             else:
-                open_position_binance_spot(pair, cc_close, cc_close, quantity, precision, SpotSides.BUY)
+                open_position_binance_spot(pair, cc_close, cc_close, quantity, SpotSides.BUY)
             return True
     else:
         if not LAST_CANDLE_RED:
@@ -304,13 +338,13 @@ def trade_the_open(pair, interval, quantity, leverage, precision, market, limit)
                 open_short_position_binance_futures(pair, targets["tp2"], cc_low, cc_close, quantity, leverage, precision)"""
         return False
 
-def main(pair, quantity, interval=Intervals.DAY, leverage=2, precision=0, market=Markets.FUTURES, limit=0):
+def main(pair, quantity, interval=Intervals.DAY, leverage=2, market=Markets.FUTURES, limit=0):
     order_filled = False
     
-    print(white.bold('* Liquidity trading of: {} with {} as amount at {} candle with x{} leverage and precision of {} at {} market starting at {} and finishing at {}.'.format(pair, quantity, interval, leverage, precision, market, START_INTERVAL, END_INTERVAL)))
+    print(white.bold('* Liquidity trading of: {} with {} as amount at {} candle with x{} leverage and at {} market starting at {} and finishing at {}.'.format(pair, quantity, interval, leverage, market, START_INTERVAL, END_INTERVAL)))
     while not TARGET_REACHED and (not order_filled or TIMES_GREEN < MAX_ORDER_RETRIES):
         if (check_open_trade_ready()):
-            order_filled = trade_the_open(pair, interval, quantity, leverage, precision, market, limit)
+            order_filled = trade_the_open(pair, interval, quantity, leverage, market, limit)
         if (not order_filled):
             time.sleep(SLEEP_TIMEOUT)
 
@@ -320,7 +354,6 @@ if __name__ == "__main__":
     parser.add_argument('--quantity', type=float, help='Quantity in USD to trade.')
     parser.add_argument('--interval', type=Intervals.from_string, choices=list(Intervals), help='Candle timeframe to trade.')
     parser.add_argument('--leverage', type=int, help='Leverage to apply on the trade.')
-    parser.add_argument('--precision', type=int, help='Number of decimals (precision) for the selected pair.')
     parser.add_argument('--market', type=Markets.from_string, help='Market where the will be executed.', default=Markets.FUTURES)
     parser.add_argument('--limit', type=float, help='Limit for spot orders.')
     parser.add_argument('--start', type=int, help='Candle UTC start.', default=0)
@@ -336,6 +369,6 @@ if __name__ == "__main__":
     END_INTERVAL = args.end
 
     MAX_STOP_LOSS_RISK = args.risk
-    main(args.pair, args.quantity, args.interval.value, args.leverage, args.precision, args.market, args.limit)
+    main(args.pair, args.quantity, args.interval.value, args.leverage, args.market, args.limit)
 
     print(green.bold('\nOrders successfully set.'))
